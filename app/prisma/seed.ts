6d40a5daeb81efb9a0fcf9bed4bcb74e031b50ec
Imad Archid <imadarchid@gmail.com>
2023-01-03 10:46:07 +0100

    :bug: fix: nasty PerformanceChart type bug

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import * as XLSX from 'xlsx'
-import { promises as fs } from 'fs'
-import path from 'path'
+// import * as XLSX from 'xlsx'
+// import { promises as fs } from 'fs'
+// import path from 'path'
 
 const prisma = new PrismaClient()
 
+const legalTypes = [{ name: 'SICAV' }, { name: 'FCP' }]
 const categories = [
   { name: 'Actions' },
   { name: 'OMLT' },
@@ -13,90 +14,107 @@ const categories = [
   { name: 'OCT' },
   { name: 'Contractuel' },
 ]
-interface Fund {
-  'Dénomination OPCVM': string
-  'CODE ISIN': string
-  'Code Maroclear': string
-  'Société de Gestion': string
-  'Nature juridique': string
-  Classification: string
-  'Périodicité VL': string
-  'Commission de souscription': number
-  ' Commission de rachat': number
-  'Frais de gestion': number | '-'
+const periodicities = [{ name: 'daily' }, { name: 'weekly' }]
+
+// interface Fund {
+//   'Dénomination OPCVM': string
+//   'CODE ISIN': string
+//   'Code Maroclear': string
+//   'Société de Gestion': string
+//   'Nature juridique': string
+//   Classification: string
+//   'Périodicité VL': string
+//   'Commission de souscription': number
+//   ' Commission de rachat': number
+//   'Frais de gestion': number | '-'
+// }
+
+// const legalHandler = (value: string) => (value === 'FCP' ? 2 : 1)
+
+// const catHandler = (value: string) =>
+//   categories.findIndex((x) => x.name === value) + 1
+
+// const periodicityHandler = (value: string) => (value === 'weekly' ? 2 : 1)
+
+// const load = async (file: string) => {
+//   const xlsxData = await fs.readFile(path.resolve(__dirname, file))
+
+//   const buffer = xlsxData
+//   const workbook = XLSX.read(buffer, { type: 'buffer' })
+//   const firstSheet = workbook.Sheets[workbook.SheetNames[0]]
+
+//   const output: Array<Fund> = XLSX.utils.sheet_to_json(firstSheet)
+
+//   return output
+// }
+
+const addLookupValues = async () => {
+  // legal types [SICAV, FCP]
+  const legalVals = await prisma.legal_types.createMany({
+    data: legalTypes,
+  })
+  // categories [Actions, OMLT, Monétaire, Diversifié, OCT, Contractuel]
+  const categoryVals = await prisma.categories.createMany({
+    data: categories,
+  })
+  // periodicities [Daily, Weekly]
+  const periodicityVals = await prisma.periodicities.createMany({
+    data: periodicities,
+  })
+
+  return [legalVals, categoryVals, periodicityVals]
 }
 
-const legalHandler = (value: string) => (value === 'FCP' ? 2 : 1)
+// const addFunds = async () => {
+//   const funds = await load('./seedFile.xlsx')
 
-const catHandler = (value: string) =>
-  categories.findIndex((x) => x.name === value) + 1
+//   for (let i = 0; i < funds.length; i++) {
+//     const manager = await prisma.managers.upsert({
+//       where: {
+//         manager_name: funds[i]['Société de Gestion'],
+//       },
+//       update: {},
+//       create: {
+//         manager_name: funds[i]['Société de Gestion'],
+//       },
+//     })
 
-const periodicityHandler = (value: string) => (value === 'weekly' ? 2 : 1)
+//     await prisma.funds.create({
+//       data: {
+//         name: funds[i]['Dénomination OPCVM'],
+//         isin_code: funds[i]['CODE ISIN'],
+//         mc_code: funds[i]['Code Maroclear'].toString(),
+//         managed_by: manager.id,
+//         legal_type: legalHandler(funds[i]['Nature juridique']),
+//         category: catHandler(funds[i].Classification),
+//         periodicity: periodicityHandler(funds[i]['Périodicité VL']),
+//       },
+//     })
 
-const load = async (file: string) => {
-  const xlsxData = await fs.readFile(path.resolve(__dirname, file))
-
-  const buffer = xlsxData
-  const workbook = XLSX.read(buffer, { type: 'buffer' })
-  const firstSheet = workbook.Sheets[workbook.SheetNames[0]]
-
-  const output: Array<Fund> = XLSX.utils.sheet_to_json(firstSheet)
-
-  return output
-}
-
-const addFunds = async () => {
-  const funds = await load('./seedFile.xlsx')
-
-  for (let i = 0; i < funds.length; i += 1) {
-    /* eslint-disable no-await-in-loop */
-    /* Ignoring no-await-in-loop because order matters */
-    const manager = await prisma.managers.upsert({
-      where: {
-        manager_name: funds[i]['Société de Gestion'],
-      },
-      update: {},
-      create: {
-        manager_name: funds[i]['Société de Gestion'],
-      },
-    })
-
-    await prisma.funds.create({
-      data: {
-        name: funds[i]['Dénomination OPCVM'],
-        isin_code: funds[i]['CODE ISIN'],
-        mc_code: funds[i]['Code Maroclear'].toString(),
-        managed_by: manager.id,
-        legal_type: legalHandler(funds[i]['Nature juridique']),
-        category: catHandler(funds[i].Classification),
-        periodicity: periodicityHandler(funds[i]['Périodicité VL']),
-      },
-    })
-
-    await prisma.rates.upsert({
-      where: {
-        isin_code: funds[i]['CODE ISIN'],
-      },
-      update: {},
-      create: {
-        subscription_fee: funds[i]['Commission de souscription'],
-        mgt_fee:
-          funds[i]['Frais de gestion'] === '-'
-            ? 0
-            : funds[i]['Frais de gestion'], // data source inconsistency
-        redemption_fee: funds[i][' Commission de rachat'],
-        funds: {
-          connect: {
-            isin_code: funds[i]['CODE ISIN'],
-          },
-        },
-      },
-    })
-  }
-}
+//     await prisma.rates.upsert({
+//       where: {
+//         isin_code: funds[i]['CODE ISIN'],
+//       },
+//       update: {},
+//       create: {
+//         subscription_fee: funds[i]['Commission de souscription'],
+//         mgt_fee:
+//           funds[i]['Frais de gestion'] === '-'
+//             ? 0
+//             : funds[i]['Frais de gestion'], // data source inconsistency
+//         redemption_fee: funds[i][' Commission de rachat'],
+//         funds: {
+//           connect: {
+//             isin_code: funds[i]['CODE ISIN'],
+//           },
+//         },
+//       },
+//     })
+//   }
+// }
 
 const seedDB = async () => {
-  await addFunds()
+  await addLookupValues()
 }
 
 seedDB()

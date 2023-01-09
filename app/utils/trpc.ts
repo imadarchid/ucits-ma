b700bb8cbf1fba4d8c75b10e50da62e2ed00e26b
Imad Archid <imadarchid@gmail.com>
2023-01-09 15:28:24 +0100

    :wrench: fix: add SSR support for tRPC

--- app/utils/trpc.ts
+++ app/utils/trpc.ts
@@ -1,5 +1,6 @@
 import { httpBatchLink } from '@trpc/client'
 import { createTRPCNext } from '@trpc/next'
+import superjson from 'superjson'
 import type { AppRouter } from '../server/routers/_app'
 
 function getBaseUrl() {
@@ -12,11 +13,41 @@ function getBaseUrl() {
 }
 
 const trpc = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
+    if (typeof window !== 'undefined') {
+      // during client requests
+      return {
+        transformer: superjson, // optional - adds superjson serialization
+        links: [
+          httpBatchLink({
+            url: '/api/trpc',
+          }),
+        ],
+      }
+    }
     return {
+      transformer: superjson,
       links: [
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                connection: _connection,
+                ...headers
+              } = ctx.req.headers
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                'x-ssr': '1',
+              }
+            }
+            return {}
+          },
         }),
       ],
     }
